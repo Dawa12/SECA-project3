5a84e23a813221eea3eae5dc46aa70d2e07b4ab7
Dawa Sherpa <dasherpa7@gmail.com>
2018-03-27 21:13:39 -0400

    update

--- front-end/react-app/src/components/Results.js
+++ front-end/react-app/src/components/Results.js
@@ -1,17 +1,79 @@
 import React, { Component } from 'react';
 
 class Results extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+
   componentWillMount() {
     const url = 'https://data.cityofnewyork.us/resource/jff5-ygbi.json';
 
     fetch(url)
       .then(res => res.json())
       .catch(error => console.error('Error:', error))
-      .then(response => console.log('Success:', response));
+      .then(response => {
+        console.log('Success:', response);
+        const data = this.state.data;
+        this.setState({ data: response });
+      });
   }
 
   render() {
-    return <div>resultsComponent</div>;
+    let tableHeaders;
+    let tableRows = [];
+    let loading = (
+      <tr>
+        <td>Loading...</td>
+      </tr>
+    );
+
+    let loadingCell = <td>Loading...</td>;
+
+    // prevent errors by checking state fetched data and has content
+    if (this.state.data.length > 0) {
+      tableHeaders = (
+        <tr>
+          <th>business name</th>
+          <th>address zip</th>
+          <th>address borough</th>
+          <th>address city</th>
+          <th>action</th>
+        </tr>
+      );
+
+      const data = this.state.data;
+
+      tableRows = data.map((value, mapIndex) => {
+        return (
+          <tr key={mapIndex}>
+            <td>{value.business_name}</td>
+            <td>{value.address_zip}</td>
+            <td>{value.address_borough}</td>
+            <td>{value.address_city}</td>
+            <td>
+              <input type="submit" value="Save" />
+            </td>
+          </tr>
+        );
+      });
+    }
+
+    return (
+      <div>
+        <form>
+          <table>
+            <tbody>
+              {tableHeaders ? tableHeaders : loading}
+              {tableRows ? tableRows : loading}
+            </tbody>
+          </table>
+          <input type="submit" />
+        </form>
+      </div>
+    );
   }
 }
 
